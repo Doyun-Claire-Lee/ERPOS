5973d3d49bbfd208bc4cbb9e928a9ffa7f5fde5f
DoyunLee <doyun.lee@kakao.com>
2020-09-22 11:54:35 +0900

    Settings

---
+++ src/main/java/com/test/erpos/TestController.java
@@ -0,0 +1,22 @@
+package com.test.erpos;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value = "/test.action", method = { RequestMethod.GET })
+	public String test(HttpServletRequest request, HttpServletResponse response) {
+
+		
+
+		return "test";
+	}
+	
+	
+}
